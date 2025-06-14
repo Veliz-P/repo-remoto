615b443cb25c6b0821c37ec35927520a90e91f32
Veliz <paucarcarlos2108@gmail.com>
2025-06-14 11:56:42 -0500

    chore: configurar api key

--- frontend_catalogo/js/firebaseIntegration.js
+++ frontend_catalogo/js/firebaseIntegration.js
@@ -7,12 +7,12 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-storage.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD-O5qNjhd-G-WUEE7jDToZe4moQvlrM_Y",
-  authDomain: "catalogo-productos-imagenes.firebaseapp.com",
-  projectId: "catalogo-productos-imagenes",
-  storageBucket: "catalogo-productos-imagenes.firebasestorage.app",
-  messagingSenderId: "321104975233",
-  appId: "1:321104975233:web:5cac9b35493d4b1d6bc480",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 // Initialize Firebase
